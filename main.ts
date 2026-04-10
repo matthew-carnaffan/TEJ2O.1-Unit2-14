060a9e2bfe13583a5ccfd85750afb9d711c1f04a
matthew-carnaffan <matthew.carnaffan@stu.ocsb.ca>
2026-04-10 13:28:46 -0400

    filled info and completed clockwise

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Matthew Carnaffan
+ * Created on: Apr 2026
+ * This program moves a sprite clockwise or counter clockwise
 */
 
-basic.showString('Hello, World!')
+//clean up 
+basic.showIcon(IconNames.Happy)
+
+// variable assign
+let loopCounter1 = 0
+let loopCounter2 = 0
+let loopCounter3 = 0
+let loopCounter4 = 0
+//set up
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+    let sprite = game.createSprite(0, 0)
+
+    //move sprite clockwise
+    while (loopCounter1 <= 5) {
+        basic.pause(500)
+        sprite.move(1)   
+        loopCounter1 = loopCounter1 + 1
+    }
+    sprite.turn(Direction.Right, 90)
+    while (loopCounter2 <= 5) {
+        basic.pause(500)
+        sprite.move(1)
+        loopCounter2 = loopCounter2 + 1
+    }
+    sprite.turn(Direction.Right, 90)
+    while (loopCounter3 <= 5) {
+        basic.pause(500)
+        sprite.move(1)
+        loopCounter3 = loopCounter3 + 1
+    }
+    sprite.turn(Direction.Right, 90)
+    while (loopCounter4 <= 5) {
+        basic.pause(500)
+        sprite.move(1)
+        loopCounter4 = loopCounter4 + 1
+    }
+    sprite.turn(Direction.Right, 90)
+})
